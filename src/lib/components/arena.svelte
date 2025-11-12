c62e9279d059ec4d8c65188271bb399a551425fb
IvanTsve <iv_plamenovv@abv.bg>
2025-11-13 00:57:15 +0200

    load sprite ninja

---
+++ src/lib/components/arena.svelte
@@ -0,0 +1,39 @@
+<div bind:this={container}>
+
+</div>
+<script >
+    import * as PIXI from 'pixi.js';
+    import {onMount} from 'svelte';
+    import run1 from '$lib/assets/run1.png';
+    import run2 from '$lib/assets/run2.png';
+    import run3 from '$lib/assets/run3.png';
+
+    let container;
+    let app;
+    let running = {
+        run1: null,
+        run2: null,
+        run3: null,
+    }
+
+    onMount(() => {
+        console.log('the component has mounted');
+        app = new PIXI.Application({
+            background: '#1099bb',
+            resizeTo: window,
+        });
+        container.appendChild(app.view);
+
+        const textures = [
+            PIXI.Texture.from(run1),
+            PIXI.Texture.from(run2),
+            PIXI.Texture.from(run3),
+        ];
+
+        const run1Sprite = new PIXI.Sprite(textures[0]);
+        app.stage.addChild(run1Sprite);
+        
+
+    });
+
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,22 @@
 <script>
-    import Button from "$lib/components/button.svelte";
-    function handleSpin() {
-        console.log("Spin button clicked!");
-    }
+    // import Button from "$lib/components/button.svelte";
+    import Arena from "$lib/components/arena.svelte";
+    // function handleSpin() {
+    //     console.log("Spin button clicked!");
+    // }
 </script>
 
 <div class="main">
-    <Button {handleSpin} />
+    <Arena />
+    <!-- <Button {handleSpin} /> -->
 </div>
 
 <style>
-    .main {
+    /* .main {
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100vh;
         background-color: #f0f0f0;
-    }
+    } */
 </style>
