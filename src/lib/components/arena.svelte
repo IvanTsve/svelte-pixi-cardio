66e230ab01ef94c4b7c7bc2a6dc20791bcc5e6c4
IvanTsve <iv_plamenovv@abv.bg>
2025-11-13 11:01:08 +0200

    add hero slash actions

--- src/lib/components/arena.svelte
+++ src/lib/components/arena.svelte
@@ -4,16 +4,28 @@
 <script >
     import * as PIXI from 'pixi.js';
     import {onMount} from 'svelte';
-    import run1 from '$lib/assets/run1.png';
-    import run2 from '$lib/assets/run2.png';
-    import run3 from '$lib/assets/run3.png';
+    import run1 from '$lib/assets/walk/run1.png';
+    import run2 from '$lib/assets/walk/run2.png';
+    import run3 from '$lib/assets/walk/run3.png';
+    import one from '$lib/assets/slash/1.png';
+    import two from '$lib/assets/slash/2.png';
+    import three from '$lib/assets/slash/3.png';
+    import four from '$lib/assets/slash/4.png';
+    import five from '$lib/assets/slash/5.png';
+    import six from '$lib/assets/slash/6.png';
 
     let container;
     let app;
     let isRunning = false;
     let frame = 0;
     let frameTicker;
-    let textures = [];
+    let textures = {
+        walk: [],
+        slash: [],
+    };
+    let slashFrame = 0;
+    let slashTicker;
+    let isSlashing = false;
 
     onMount(() => {
         console.log('the component has mounted');
@@ -22,13 +34,21 @@
             resizeTo: window,
         });
         container.appendChild(app.view);
-        textures.push(
+        textures['walk'].push(
             PIXI.Texture.from(run1),
             PIXI.Texture.from(run2),
             PIXI.Texture.from(run3),
         );
+        textures['slash'].push(
+            PIXI.Texture.from(one),
+            PIXI.Texture.from(two),
+            PIXI.Texture.from(three),
+            PIXI.Texture.from(four),
+            PIXI.Texture.from(five),
+            PIXI.Texture.from(six),
+        );
 
-        const hero = new PIXI.Sprite(textures[1]);
+        const hero = new PIXI.Sprite(textures['walk'][1]);
         app.stage.addChild(hero);
 
 
@@ -39,15 +59,42 @@
             if (e.code === 'KeyA') {
                 handleLeft(hero);
             }
+            if (e.code == 'Space') {
+                handleSlash(hero);
+            }
         });
 
         window.addEventListener('keyup', (e) => {
             if (e.code === 'KeyD' || e.code === 'KeyA') {
                 stopWalk(hero);
             }
+            if (e.code == 'Space') {
+                setTimeout(() => {
+                    clearInterval(slashTicker);
+                    isSlashing = false;
+                    slashFrame = 0;
+                    hero.texture = textures['walk'][0];
+                }, 100);
+            }
         });
 
     });
+    function handleSlash(hero) {
+        hero.scale.x = 1;
+        hero.anchor.x = 0;
+        if (isSlashing) return;
+        isSlashing = true;
+        slashTicker = setInterval(() => {
+            slashFrame = (slashFrame +1) % textures['slash'].length;
+            hero.texture = textures['slash'][slashFrame];
+        },50) 
+        
+
+            
+
+
+    }
+
     function handleRight(hero) {
         hero.scale.x = 1;
         hero.anchor.x = 0;
@@ -66,15 +113,15 @@
         isRunning = true;
 
         frameTicker = setInterval(() => {
-            frame = (frame + 1) % textures.length;
-            hero.texture = textures[frame];
+            frame = (frame + 1) % textures['walk'].length;
+            hero.texture = textures['walk'][frame];
         }, 200);
     }
 
     function stopWalk(hero) {
         isRunning = false;
         clearInterval(frameTicker);
-        hero.texture = textures[0];
+        hero.texture = textures['walk'][0];
     }
 
 
